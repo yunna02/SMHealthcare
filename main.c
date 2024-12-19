88f47f7fcf4429ad15d9b4246a9e5b1fc83ffc76
yunna02 <cyndbssk@sookmyung.ac.kr>
2024-12-20 03:02:42 +0900

    comment3

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ void inputExercise(HealthData* health_data);
 // Function prototype for inputDiet
 void inputDiet(HealthData* health_data);
 
+// Function prototype for calculating remaining calories
 int calculatecal_L(const HealthData* health_data);
 
 int main() {
@@ -38,8 +39,9 @@ int main() {
 
     // ToCode: to run the "Healthcare Management Systems" until all calories are used up or the user wants to exit the system
     do {
-		int cal_L = calculatecal_L(&health_data);
-
+		int cal_L = calculatecal_L(&health_data); // Calculate remaining calories
+	
+		// If remaining calories are zero, exit the system
 		if (cal_L == 0){
 			printf("You have consumed all your calories for today! \n");
 			saveData(HEALTHFILEPATH, &health_data); // Save data before exiting
@@ -48,6 +50,7 @@ int main() {
 			return 0; // Exit the system
 		} 
 		
+		//Display menu
 		printf("\n=======================================================================\n");
         printf("[Healthcare Management Systems] \n");
         printf("1. Exercise \n");
@@ -55,37 +58,37 @@ int main() {
         printf("3. Show logged information \n");
         printf("4. Exit \n");
         printf("Select the desired number: ");
-        scanf("%d", &choice);
+        scanf("%d", &choice); //user's choice read
         printf("=======================================================================\n");
     
         
 		// ToCode: to run the sysmtem based on the user's choice
         switch (choice) {
-            case 1:
-            	inputExercise(&health_data);
-				saveData(HEALTHFILEPATH, &health_data); 
+            case 1: //Input Exercise and Save data
+            	inputExercise(&health_data); //Add selected exercise, calculate calories burned to health data
+				saveData(HEALTHFILEPATH, &health_data); //Store updated health data
                 break;
                 
-            case 2:
-            	inputDiet(&health_data);
-				saveData(HEALTHFILEPATH, &health_data);
+            case 2: //Input Diets and Save data
+            	inputDiet(&health_data); //Adds selected food's calories to health data
+				saveData(HEALTHFILEPATH, &health_data); //Store updated health data
                 break;
                 
-            case 3:
-            	printHealthData(&health_data);
+            case 3: //Output health data
+            	printHealthData(&health_data); //Watch exercise, diet, and remaining calories.
                 break;
                 
-            case 4:
-            	saveData(HEALTHFILEPATH, &health_data);
+            case 4: //Exit
+            	saveData(HEALTHFILEPATH, &health_data); //Save health data before exiting.
 				printf("Exit the system.\n");
 				printf("=======================================================================\n");
                 return 0;
                 
-            default:
+            default: //Handle Invalid input
                 printf("[Error] Invalid option. \n");
                 printf("Please try again! \n");
         }
-    } while (1);
+    } while (1); // Continue until pick to exit(4) or Remaining calories = 0
     
 	
 	return 0;
